e05d314a754050d251eec1ed34ce663f3d63ea37
Julia Caine <ariandjuliagetmarried@gmail.com>
2017-12-20 20:45:01 +0000

    Added url back in

--- jcam/jcam/urls.py
+++ jcam/jcam/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import url, include, path
+from django.urls import include, path
 from django.contrib import admin
+from django.conf.urls import url
 
 urlpatterns = [
     path(r'/', include('weddingsite.urls')),
