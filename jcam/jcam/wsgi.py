21aba892fb4eafcadd6ce4d5efdf3bef85a2255a
Julia Caine <ariandjuliagetmarried@gmail.com>
2017-12-20 18:36:47 +0000

    changed import for get_wsgi

--- jcam/jcam/wsgi.py
+++ jcam/jcam/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jcam.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jcam.jcam.settings")
 
 application = get_wsgi_application()
