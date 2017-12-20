18030dfda1153e6522ede6b78adeb828242f63d1
Julia Caine <ariandjuliagetmarried@gmail.com>
2017-12-20 19:55:45 +0000

    test change static part 2

--- jcam/jcam/settings.py
+++ jcam/jcam/settings.py
@@ -18,11 +18,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static_afjlasfa/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_adfasfa'),
 )
 
 # Quick-start development settings - unsuitable for production
