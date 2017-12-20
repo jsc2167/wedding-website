1930c22fafbdc6d977e521a6f6638e1f09f794ca
Julia Caine <ariandjuliagetmarried@gmail.com>
2017-12-20 20:06:21 +0000

    Move databases

--- jcam/jcam/settings.py
+++ jcam/jcam/settings.py
@@ -13,10 +13,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
-
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -100,6 +96,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
