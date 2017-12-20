bc7ef6b7c0b80741ed7ffc2fd22710d9424399e2
Julia Caine <ariandjuliagetmarried@gmail.com>
2017-12-20 19:52:12 +0000

    Removed wedding site apps

--- jcam/jcam/settings.py
+++ jcam/jcam/settings.py
@@ -47,8 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'weddingsite.apps.BlogConfig',
-    'weddingsite'
 ]
 
 MIDDLEWARE = [
