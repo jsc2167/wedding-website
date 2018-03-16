073f18c55941e07519d75644845065e4636ae5f0
Julia Caine <ariandjuliagetmarried@gmail.com>
2018-03-16 21:44:57 +0000

    After git pull

--- jcam/settings.py
+++ jcam/settings.py
@@ -16,7 +16,7 @@ import os
 # settings.configure(DEBUG=True)
 # import django
 # django.setup()
-
+ 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
